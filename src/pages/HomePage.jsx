b142e51db73c6462eb49a47eaadc6f8dfb2b3945
PieMenard <pinedo.m.gustavo@gmail.com>
2025-05-26 23:47:00 -0400

    add loading and error states to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,27 @@ import { useEffect, useState } from 'react';
 
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
-import { Separator } from '@/components/ui';
+import { Separator, Spinner } from '@/components/ui';
 import api from '@/api';
 
 const HomePage = () => {
   const [listings, setListings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchListings = async () => {
-      const response = await api.get('/api/listings');
-      setListings(response.data);
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await api.get('/api/listings');
+        setListings(response.data);
+      } catch {
+        setError('Something went wrong. PLease try again later.');
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchListings();
@@ -21,13 +32,29 @@ const HomePage = () => {
     //will implement later
   };
 
+  const renderListingList = () => {
+    if (isLoading) {
+      return (
+        <div className='flex justify-center'>
+          <Spinner size='sm' />
+        </div>
+      );
+    }
+
+    if (error) {
+      return <div className='text-center'>{error}</div>;
+    }
+
+    return <ListingList listings={listings} />;
+  };
+
   return (
     <div className='container py-4'>
       <div className='mb-4'>
         <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
       </div>
-      <ListingList listings={listings} />
+      {renderListingList()}
     </div>
   );
 };
