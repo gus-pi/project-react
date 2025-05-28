c98d51045300f3b9913e12b9302f241cc0fa0dd5
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-05-28 09:38:58 -0400

    add filters to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,11 @@ const HomePage = () => {
   const [listings, setListings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [filters, setFilters] = useState({
+    dates: undefined,
+    guests: 0,
+    search: '',
+  });
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -16,20 +21,20 @@ const HomePage = () => {
       setError(null);
 
       try {
-        const response = await api.get('/api/listings');
+        const response = await api.get('/api/listings', { params: filters });
         setListings(response.data);
       } catch {
-        setError('Something went wrong. PLease try again later.');
+        setError('Something went wrong. Please try again later.');
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchListings();
-  }, []);
+  }, [filters]);
 
   const handleFilters = (filters) => {
-    //will implement later
+    setFilters(filters);
   };
 
   const renderListingList = () => {
