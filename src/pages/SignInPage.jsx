2dc82fa8b3e0a6e2bc2da32cc2e6b11e38e95e78
PieMenard <pinedo.m.gustavo@gmail.com>
2025-06-08 18:23:15 -0400

    add sign out in navbar and redirect to home when signed in

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,6 +1,18 @@
+import { useAuth } from '@/components/AuthProvider';
 import SignInForm from '@/components/SignInForm';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignInPage = () => {
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      navigate('/', { replace: true });
+    }
+  }, [navigate, token]);
+
   return (
     <div className='container flex h-screen items-center justify-center py-4'>
       <SignInForm />
