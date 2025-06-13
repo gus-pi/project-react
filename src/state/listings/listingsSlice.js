0397869da6bc563d8bce349da30e31cfc73e1f01
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-06-13 14:30:00 -0400

    remove fetching from redux slitingsSlice and delete useFetch hook

--- src/state/listings/listingsSlice.js
+++ src/state/listings/listingsSlice.js
@@ -1,5 +1,4 @@
-import api from '@/api';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   listings: [],
@@ -21,33 +20,7 @@ const listingsSlice = createSlice({
       );
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchListings.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(fetchListings.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.listings = action.payload;
-      })
-      .addCase(fetchListings.rejected, (state, action) => {
-        const message = action.error.message;
-
-        if (message !== 'Aborted') {
-          state.status = 'failed';
-          state.error = message;
-        }
-      });
-  },
 });
-
-export const fetchListings = createAsyncThunk(
-  'listings/fetchListings',
-  async (options) => {
-    const response = await api.get('/api/listings', options);
-    return response.data;
-  },
-);
 
 export const { addFavoriteListing, removeFavoriteListing } =
   listingsSlice.actions;
