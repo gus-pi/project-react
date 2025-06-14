695f9f52316405b6abb8d1178e0c67276f8df56e
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-06-14 12:42:33 -0400

    set users slice

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import listingsReducer from './listings/listingsSlice';
+import usersReducer from './users/usersSlice';
 
 export const store = configureStore({
   reducer: {
     listings: listingsReducer,
+    users: usersReducer,
   },
 });
