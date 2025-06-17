586832f34b1ebab6017fa72ae02f3ff777f5e891
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-06-17 16:15:36 -0400

    add listing rating stars component

---
+++ src/components/ListingRatingStars.jsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils/cn';
+import { Star } from 'lucide-react';
+
+const ListingRatingStars = ({ className, listing }) => {
+  return (
+    <div
+      className={cn(
+        'inline-flex flex-row items-center rounded-md bg-background px-2 py-1',
+        className,
+      )}
+    >
+      <Star className='h-4 w-4 fill-star text-star' />
+      <span className='ml-2'>{listing.rating}</span>
+    </div>
+  );
+};
+
+export default ListingRatingStars;
