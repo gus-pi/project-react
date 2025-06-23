855d0c8662c89a1cdf1bfb46c084ce7766a6da14
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-06-23 15:41:00 -0400

    test for correct number of initial listings

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -11,4 +11,12 @@ describe('template spec', () => {
     cy.get('[data-testid="listing-filters"]').should('exist');
     cy.get('[data-testid="listing-list"]').should('exist');
   });
+
+  it('displays the correct number of initial listings', () => {
+    const expectedListingCount = 12;
+    cy.get('[data-testid="listing-list"] > *').should(
+      'have.length',
+      expectedListingCount,
+    );
+  });
 });
